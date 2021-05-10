f3c2b2de6b4a4884022a49861482ff3b8a48f523
Josmar-jr <josmartrigueiro.jr@gmail.com>
2021-05-10 12:44:58 -0300

    third commit: useCallback()

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import { useEffect, useState } from 'react';
+import P from 'prop-types';
+import React, { useCallback, useState } from 'react';
+
+const Button = React.memo(function Button({ incrementButton }) {
+  console.log('children render');
+  return <button onClick={() => incrementButton(10)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementButton: P.func,
+};
 
 export default function App() {
   const [counter, setCounter] = useState(0);
 
-  const handleCounter = () => {
-    setCounter((c) => c + 1);
-  };
-
-  // componentDidUpdate - executa toda vez que o component atualiza
-  useEffect(() => {
-    console.log(`hello world`);
-  });
-  // componentDidMount - executa 1x
-  useEffect(() => {
-    console.log(`run 1x`);
+  const incrementCounter = useCallback((num) => {
+    setCounter((c) => c + num);
   }, []);
-  // Com dependecia - executa td vez que a dependencia mudar
-  useEffect(() => {
-    console.log(`counter change for ${counter}`);
-  }, [counter]);
+
+  console.log('father render');
 
   return (
     <div>
       <h1>{counter}</h1>
-      <button onClick={handleCounter}>increment</button>
+      <Button incrementButton={incrementCounter} />
     </div>
   );
 }
