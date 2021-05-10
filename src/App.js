ecada50505bd62329471aa002ef2ef3857035b87
Josmar-jr <josmartrigueiro.jr@gmail.com>
2021-05-10 12:16:10 -0300

    second commit: useEffect()

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
   const [counter, setCounter] = useState(0);
-  const [color, setColor] = useState(true);
 
   const handleCounter = () => {
     setCounter((c) => c + 1);
-    setColor((f) => f === false);
   };
 
-  console.log(color);
+  // componentDidUpdate - executa toda vez que o component atualiza
+  useEffect(() => {
+    console.log(`hello world`);
+  });
+  // componentDidMount - executa 1x
+  useEffect(() => {
+    console.log(`run 1x`);
+  }, []);
+  // Com dependecia - executa td vez que a dependencia mudar
+  useEffect(() => {
+    console.log(`counter change for ${counter}`);
+  }, [counter]);
 
   return (
     <div>
-      <h1 styles={{ backgroundColor: `${color} === true ? 'red' : 'blue'` }}>{counter}</h1>
+      <h1>{counter}</h1>
       <button onClick={handleCounter}>increment</button>
     </div>
   );
