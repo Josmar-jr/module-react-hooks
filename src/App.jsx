0d79bdd60a16b59e7667f0988133a8c623826cbf
Josmar-jr <josmartrigueiro.jr@gmail.com>
2021-05-10 13:56:43 -0300

    five commit: useRef()

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import P from 'prop-types';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
   console.log('Filho renderizou')
   return (
     <div key={post.id}>
-      <h1>{post.title}</h1>
+      <h1
+        style={{ fontSize: '14px' }}
+        onClick={() =>
+          handleClick(post.title)}>{post.title}
+      </h1>
     </div>
   );
 };
@@ -17,20 +21,31 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.func,
 };
 
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
 
   // Component did mount
   useEffect(() => {
-    setTimeout(() => {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((r) => r.json())
-        .then((r) => setPosts(r));
-    }, 5000);
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.json())
+      .then((r) => setPosts(r));
+
   }, []);
+
+  useEffect(() => {
+    input.current.focus()
+    console.log(input.current)
+  }, [value])
+
+  const handleClick = (value) => {
+    setValue(value)
+  }
 
   console.log('renderinzou');
 
@@ -38,6 +53,7 @@ export default function App() {
     <div>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -47,7 +63,7 @@ export default function App() {
 
       {useMemo(() => {
         return posts.length > 0 && (
-          posts.map((post) => <Post key={post.id} post={post} />)
+          posts.map((post) => <Post key={post.id} post={post} handleClick={handleClick} />)
         )
       }, [posts])}
 
